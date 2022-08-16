d8ecb10779502f0f7fa008e223248778afc7a3c1
Romka <prostoonado@gmail.com>
2022-08-16 22:02:37 +0300

    FiltersView

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import FilterView from './view/filters-view.js';
+import FiltersView from './view/filters-view.js';
 import { render } from './render.js';
 import ContentPresenter from './presenter/content-presenter.js';
 
@@ -7,6 +7,6 @@ const siteHeaderElement = document.querySelector('.trip-controls__filters');
 const siteMainElement = document.querySelector('.trip-events');
 const contentPresenter = new ContentPresenter();
 
-render(new FilterView(), siteHeaderElement);
+render(new FiltersView(), siteHeaderElement);
 
 contentPresenter.init(siteMainElement);
