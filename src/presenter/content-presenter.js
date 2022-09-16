9f098a39039f784ff22b67d3c6fd6f725ad9f720
Romka <prostoonado@gmail.com>
2022-09-16 19:42:52 +0300

    module3-task1

--- src/presenter/content-presenter.js
+++ src/presenter/content-presenter.js
@@ -5,19 +5,57 @@ import EventsListView from '../view/events-list-view.js';
 import WaypointView from '../view/waypoint-view.js';
 
 export default class ContentPresenter {
-  eventsListComponent = new EventsListView();
+  #eventsListComponent = new EventsListView();
+  #boardContainer = null;
+  #waypointsModel = null;
+  #listWaypoints = [];
 
   init = (boardContainer, waypointsModel,) => {
-    this.waypointsModel = waypointsModel;
-    this.listWaypoints = [...this.waypointsModel.getWaypoints()];
+    this.#boardContainer = boardContainer;
+    this.#waypointsModel = waypointsModel;
+    this.#listWaypoints = [...this.#waypointsModel.waypoints];
 
-    render(new SortingView(), boardContainer);
-    render(this.eventsListComponent, boardContainer);
+    render(new SortingView(), this.#boardContainer);
 
-    render(new FormOfEditingView(), this.eventsListComponent.getElement());
+    render(this.#eventsListComponent, this.#boardContainer);
 
-    for (let i = 0; i < this.listWaypoints.length; i++) {
-      render(new WaypointView(this.listWaypoints[i]), this.eventsListComponent.getElement());
+    // render(new FormOfEditingView(this.#listWaypoints[0]), this.#eventsListComponent.element);
+
+    for (let i = 0; i < this.#listWaypoints.length; i++) {
+      this.#renderWaypoint(this.#listWaypoints[i]);
     }
+  };
+
+  #renderWaypoint = (waypoint) => {
+    const waypointComponent = new WaypointView(waypoint);
+
+    const formOfEditingComponent = new FormOfEditingView(waypoint);
+
+    const replaceWaypointToForm = () => {
+      this.#eventsListComponent.element.replaceChild(formOfEditingComponent.element, waypointComponent.element);
+    };
+
+    const replaceFormToWaypoint = () => {
+      this.#eventsListComponent.element.replaceChild(waypointComponent.element, formOfEditingComponent.element);
+    };
+    const onEscKeyDown = (evt) => {
+      if (evt.key === 'Escape' || evt.key === 'Esc') {
+        evt.preventDefault();
+        replaceFormToWaypoint();
+        document.removeEventListener('keydown', onEscKeyDown);
+      }
+    };
+    waypointComponent.element.querySelector('.event__rollup-btn').addEventListener('click', () => {
+      replaceWaypointToForm();
+      document.addEventListener('keydown', onEscKeyDown);
+    });
+
+    formOfEditingComponent.element.querySelector('form').addEventListener('submit', (evt) => {
+      evt.preventDefault();
+      replaceFormToWaypoint();
+      document.removeEventListener('keydown', onEscKeyDown);
+    });
+
+    render(waypointComponent, this.#eventsListComponent.element);
   };
 }
