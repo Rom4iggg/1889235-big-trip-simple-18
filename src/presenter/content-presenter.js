df16146276e29df417e9f6428acd273f8a1f4043
Romka <prostoonado@gmail.com>
2022-10-04 16:28:29 +0300

    Revert "Разделение презентора"
    This reverts commit 99f415f5f3cd0ea6edd7d4bfcd0eda96f5361789.

---
+++ src/presenter/content-presenter.js
@@ -0,0 +1,88 @@
+import { render, replace } from '../framework/render.js';
+import SortingView from '../view/sorting-view.js';
+import FormOfEditingView from '../view/form-of-editing-view.js';
+import EventsListView from '../view/events-list-view.js';
+import WaypointView from '../view/waypoint-view.js';
+import EmptyListView from '../view/empty-list-view.js';
+
+export default class ContentPresenter {
+  #eventsListComponent = new EventsListView();
+  #boardContainer = null;
+  #waypointsModel = null;
+  #listWaypoints = [];
+
+  constructor(boardContainer, waypointsModel) {
+    this.#boardContainer = boardContainer;
+    this.#waypointsModel = waypointsModel;
+  }
+
+  init = () => {
+    this.#listWaypoints = [...this.#waypointsModel.waypoints];
+
+    this.#renderList();
+  };
+
+  #renderWaypoint = (waypoint) => {
+    // this.#listWaypoints = [...this.#waypointsModel.waypoints];
+
+    const waypointComponent = new WaypointView(
+      waypoint,
+      this.#waypointsModel.getWaypointOffers(waypoint),
+      this.#waypointsModel.getWaypointDestinations(waypoint)
+    );
+
+    const formOfEditingComponent = new FormOfEditingView(
+      waypoint,
+      this.#waypointsModel.allOffers,
+      this.#waypointsModel.allDestinations
+    );
+
+    const replaceWaypointToForm = () => {
+      replace(formOfEditingComponent, waypointComponent);
+    };
+
+    const replaceFormToWaypoint = () => {
+      replace(waypointComponent, formOfEditingComponent);
+    };
+
+    const onEscKeyDown = (evt) => {
+      if (evt.key === 'Escape' || evt.key === 'Esc') {
+        evt.preventDefault();
+        replaceFormToWaypoint();
+        document.removeEventListener('keydown', onEscKeyDown);
+      }
+    };
+
+    waypointComponent.setClickHandler(() => {
+      replaceWaypointToForm();
+      document.addEventListener('keydown', onEscKeyDown);
+    });
+
+    formOfEditingComponent.setSubmitHandler(() => {
+      replaceFormToWaypoint();
+      document.removeEventListener('keydown', onEscKeyDown);
+    });
+
+    formOfEditingComponent.setClickHandler(() => {
+      replaceFormToWaypoint();
+      document.removeEventListener('keydown', onEscKeyDown);
+    });
+
+    render(waypointComponent, this.#eventsListComponent);
+  };
+
+  #renderList = () => {
+    if (!(this.#listWaypoints.length > 0)) {
+      render(new EmptyListView(), this.#boardContainer);
+    } else {
+
+      render(new SortingView(), this.#boardContainer);
+
+      render(this.#eventsListComponent, this.#boardContainer);
+
+      for (let i = 0; i < this.#listWaypoints.length; i++) {
+        this.#renderWaypoint(this.#listWaypoints[i]);
+      }
+    }
+  };
+}
